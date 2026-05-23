19851545e19463586badaff80e89d4d46a8d17ef
MahendraSiddamurthy <mahendrasiddamurthi@gmail.com>
2026-05-23 21:22:03 +0530

    Learned java basics variable and primitive data types

---
+++ java_basics/ConsistencyPromise.java
@@ -0,0 +1,13 @@
+public class ConsistencyPromise {
+
+    public static void main(String args[]) {
+        System.out.println("I promise to be consistent in my learning and coding journey.");
+
+        System.out.println("My Java journey has started");
+
+        System.out.println("I will stay consistent");
+
+        System.out.println("Daily Java + Daily DSA");
+    }
+}
+   
---
+++ java_basics/VariablesDemo.java
@@ -0,0 +1,17 @@
+public class VariablesDemo{
+
+    public static void main(String args[]){
+
+        int age = 26;
+        String name = "Mahendra Reddy";
+        char initial = 'S';
+        double weight = 78.0;
+        boolean isConsistent = true;
+
+        System.out.println(initial);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(isConsistent);
+    }
+}
